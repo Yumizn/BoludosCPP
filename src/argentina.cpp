a42556da67af77a3ee44a425a9fa2f8f3423cac6
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-03-17 02:22:56 -0300

    Update argentina.cpp

--- src/argentina.cpp
+++ src/argentina.cpp
@@ -16,7 +16,10 @@ using namespace std;
 void argentina()
 {
 	std::cout << "Hola Manco de mierda" << std::endl;
-	std::cout << "Las Islas Malvinas son de Inglaterra!" << std::endl;
+	std::cout << "El juego consiste en sumar puntos, ¡quien haga mas puntos gana!" << std::endl;
 	std::cout << "En 15 anos de Left 4 Dead aun no han aprendido a jugar" << std::endl;
+	std::cout << "rushear es la forma correcta de ganar!" << std::endl;
+	std::cout << "Cuanto antes llegues a la casa segura,mayor será la garantía de ganar el juego" << std::endl;
+	std::cout << "argentino vive en un pais en bancarrota donde 99999999 pesos no valen 1 real" << std::endl;
 }
 
