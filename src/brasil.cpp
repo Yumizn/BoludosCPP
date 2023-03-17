e3fa944b30b1d44281dcd5db5d11ec9a1fff4954
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-03-17 02:37:11 -0300

    Update brasil.cpp

--- src/brasil.cpp
+++ src/brasil.cpp
@@ -16,7 +16,7 @@ void brasil()
 {
   std::cout << "Eai Favelado Fudido!" << std::endl;
   std::cout << "Joga Direito seu Petista de merda!" << std::endl;
-  std::cout << "Nem noé carregou tanto animal como um brasileiro jogando" << std::endl;
+  std::cout << "Nem noe carregou tanto animal como um brasileiro jogando" << std::endl;
   std::cout << "Sem Rushar Sem Vitoria" << std::endl;
   std::cout << "Para de colocar a porra da foto da sua cara de fuinha na steam porque ninguem quer ver sua cara fudida" << std::endl;
   std::cout << "A maioria dos brasileiros usam Cheats e Hacks e TODOS perdem usando isso incompetentes Imundos" << std::endl;
