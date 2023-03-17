838d5c0f3359c61d57740e09598a4637370505cd
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-03-17 02:24:35 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ using namespace std;
 int main()
 {
 	std::cout << "Welcome to a LEFT FOR DEAD 2 COACHING FOR MONKEYS!" << std::endl << std::endl;
+	std::cout << "All These Tips/Tricks are valid for you who only play dead center and when they are caught, they quit the match" << std::endl;
 		menu();
 	
 	
