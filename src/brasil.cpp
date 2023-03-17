2225e8518ab5578360b033e78c48a76cf48ec727
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-03-17 02:31:28 -0300

    Create brasil.cpp

---
+++ src/brasil.cpp
@@ -0,0 +1,23 @@
+// Includes &/or Headers
+
+#include <iostream>
+#include <ostream>
+#include "menu.hpp"
+#include "brasil.hpp"
+
+
+// Namespaces/Workspaces
+
+using namespace std;
+
+
+
+void brasil()
+{
+	std::cout << "Eai Favelado Fudido!" << std::endl;
+	std::cout << "Joga Direito seu Petista de merda!" << std::endl;
+	std::cout << "Nem noé carregou tanto animal como um brasileiro jogando" << std::endl;
+  std::cout << "Sem Rushar Sem Vitoria" << std::endl;
+  std::cout << "Para de colocar a porra da foto da sua cara de fuinha na steam porque ninguem quer ver sua cara fudida" << std::endl;
+  std::cout << "A maioria dos brasileiros usam Cheats e Hacks e TODOS perdem usando isso incompetentes Imundos" << std::endl;
+}
