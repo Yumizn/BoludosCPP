37c621a21c0d88d6eb60dcae0c18b0b1082afd97
Lord Griffo <120344319+LordGriffo@users.noreply.github.com>
2023-03-21 14:42:48 -0300

    Add files via upload
    Add an chilean Function!

---
+++ src/chile.cpp
@@ -0,0 +1,25 @@
+// Includes &/or Headers
+
+#include <iostream>
+#include <ostream>
+#include "menu.hpp"
+#include "chile.hpp"
+
+
+
+// Namespaces/Workspaces
+
+using namespace std;
+
+
+
+void chile()
+{
+	std::cout << "Hola Manco de mierda" << std::endl;
+	std::cout << "1 real = 999999999999 pesos!" << std::endl;
+	std::cout << "tan jodido cuando los argentinos!" << std::endl;
+	std::cout << "Chile es un pais zombie, nadie quiere vivir alli, ni siquiera tu" << std::endl;
+	std::cout << "¡Las mujeres chilenas son tan feas que os llevaran a los chilenos a la extincion!" << std::endl;
+	std::cout << "Si no lo creen, miren a sus madres!" << std::endl;
+	std::cout << "y ademas de todo eso sois tan perdedores como los argentinos el resto de latinoamerica y el mundo os ve como unos memes hilarantes" << std::endl;
+}
